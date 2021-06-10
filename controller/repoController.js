0b289a1ab0ec7422c5473faf96db6e2bac681b6e
masaditya <aeka489@gmail.com>
2021-06-10 18:27:00 +0700

    repo

--- controller/repoController.js
+++ controller/repoController.js
@@ -1,38 +1,40 @@
-const Repo = require('../model/Repo')
+const Repo = require("../model/Repo");
 const mongoose = require("mongoose");
 
 module.exports = {
-  index: function(req, res) {
-    Repo.find().then(function(rows) {
-      res.send(rows)
-    })
+  index: function (req, res) {
+    Repo.find().then(function (rows) {
+      res.send(rows);
+    });
   },
 
-  show: function(req, res) {
-    Repo.findById(req.params.id).then(function(row) {
-      res.send(row)
-    })
+  show: function (req, res) {
+    Repo.find({ code_dosen: req.params.id }).then(function (row) {
+      res.send(row);
+    });
   },
 
-  store: function(req, res) {
-    Repo.create({ ...req.body, _id: new mongoose.Types.ObjectId() }).then(function(row) {
-      res.send(row)
-    })
+  store: function (req, res) {
+    Repo.create({ ...req.body, _id: new mongoose.Types.ObjectId() }).then(
+      function (row) {
+        res.send(row);
+      }
+    );
   },
 
-  update: function(req, res) {
+  update: function (req, res) {
     Repo.findOneAndUpdate(
       { id: req.params.id },
       { $set: req.body },
       { new: true }
-    ).then(function(row) {
-      res.send(row)
-    })
+    ).then(function (row) {
+      res.send(row);
+    });
   },
 
-  destroy: function(req, res) {
-    Repo.findOneAndDelete({ id: req.params.id }).then(function(row) {
-      res.send(row)
-    })
-  }
-}
+  destroy: function (req, res) {
+    Repo.findOneAndDelete({ id: req.params.id }).then(function (row) {
+      res.send(row);
+    });
+  },
+};
